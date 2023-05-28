81e19d6d5938e2fa68520c73d54db027dc21e7a1
itzraisu <133676178+itzraisu@users.noreply.github.com>
2023-05-28 09:40:34 -0300

    Update r.sh

--- r.sh
+++ r.sh
@@ -1,3 +1,7 @@
+apt install sudo
+pkg install sudo
+apt-get install sudo
+yum install sudo
 curl -sL https://deb.nodesource.com/setup_19.x | sudo -E bash -
 sudo apt-get install -y nodejs
 npm install -g vite
