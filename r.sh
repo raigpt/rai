812461a027b8b6b6cfa406cf8540b476bb70a64c
itzraisu <133676178+itzraisu@users.noreply.github.com>
2023-05-28 10:46:44 -0300

    Update r.sh

--- r.sh
+++ r.sh
@@ -1,18 +1,6 @@
-apt install git
-apt install sudo
-git clone https://github.com/sudo-project/sudo.git
-cd sudo
-./configure
-make
-make install
-sudo -V
-sudo apt-get update
-sudo apt-get install curl
 npm install -g vite
 npm ci
 npm run frontend
-sudo apt-get update
-sudo apt-get install curl
 curl -L https://install.meilisearch.com | sh
 ./meilisearch --master-key cdK34y2g4YlcWvdmF1dGGqkx8zRpkpGYmfpn1LjosLfi
 npm run backend
