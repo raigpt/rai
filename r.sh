c857cd9bbd9b9cc0795d78a31fe96b3b442fea8b
itzraisu <133676178+itzraisu@users.noreply.github.com>
2023-05-28 09:22:44 -0300

    Update r.sh

--- r.sh
+++ r.sh
@@ -3,5 +3,8 @@ nano .env.example .env
 cd ..
 npm ci
 npm run frontend
+$ sudo apt-get update
+$ sudo apt-get install curl
+$ curl -L https://install.meilisearch.com | sh
 ./meilisearch --master-key cdK34y2g4YlcWvdmF1dGGqkx8zRpkpGYmfpn1LjosLfi
 npm run backend
