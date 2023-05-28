456fb16f93089789c0ced7810b4765abca513049
itzraisu <133676178+itzraisu@users.noreply.github.com>
2023-05-28 09:38:41 -0300

    Update r.sh

--- r.sh
+++ r.sh
@@ -1,10 +1,10 @@
-$ curl -sL https://deb.nodesource.com/setup_19.x | sudo -E bash -
-$ sudo apt-get install -y nodejs
-$ npm install -g vite
+curl -sL https://deb.nodesource.com/setup_19.x | sudo -E bash -
+sudo apt-get install -y nodejs
+npm install -g vite
 npm ci
 npm run frontend
-$ sudo apt-get update
-$ sudo apt-get install curl
-$ curl -L https://install.meilisearch.com | sh
+sudo apt-get update
+sudo apt-get install curl
+curl -L https://install.meilisearch.com | sh
 ./meilisearch --master-key cdK34y2g4YlcWvdmF1dGGqkx8zRpkpGYmfpn1LjosLfi
 npm run backend
